777e913e71d3231cd2b5fc7a1c12220d5d062283
Rosi Sarrazin <111696869+27sarrazin@users.noreply.github.com>
2023-10-05 15:05:51 +0000

    adicionando tags

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -7,6 +7,13 @@ export default function Card(props){ //minha funcao esta sendo importada
     return (
         <div className="card"> 
         <h2>{item.nome}</h2>
+
+        <div className='tags-list'>
+            <div className='tag'>texto qualquer</div>
+            <div className='tag'>texto qualquer</div>
+            <div className='tag'>texto qualquer</div>
+            <div className='tag'>texto qualquer</div>
+        </div>
         <img src={item.imageUrl} alt="Rick"/>
       </div>
     )
